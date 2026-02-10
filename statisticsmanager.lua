155fddf1e6bd42a82df9716e6751899b199db2b8
Jamemes <56838048+Jamemes@users.noreply.github.com>
2026-02-10 20:29:44 +0300

    7.3

--- statisticsmanager.lua
+++ statisticsmanager.lua
@@ -1,11 +1,31 @@
 if type(StatisticsManager.completed_job) == "function" then
 	local data = StatisticsManager.completed_job
 	function StatisticsManager:completed_job(job_id, difficulty, require_one_down)
-		local completed_job = tweak_data.achievement.jobs_to_complete_by_achievements[job_id .. "_" .. difficulty .. (require_one_down and "_OD" or "")]
-		if Global.blackmarket_manager.IngameAchievments[completed_job] then
+		local stat_name = "completed"
+		stat_name = stat_name .. "_" .. tostring(job_id)
+		stat_name = stat_name .. "_" .. tostring(difficulty)
+		stat_name = stat_name .. (require_one_down and "_od" or "")
+		if Global.blackmarket_manager.IngameAchievments[stat_name] then
 			return data(self, job_id, difficulty, require_one_down)
 		else
 			return 0
 		end
 	end
+
+	Hooks:PreHook(StatisticsManager, "stop_session", "IngameAchievments.StatisticsManager.stop_session.PostHook", function(self, data)
+		local job_id = managers.job:current_job_id()
+		local can_record_session = job_id and data and true or false
+		can_record_session = can_record_session and not (managers.mutators and managers.mutators:should_disable_statistics())
+
+		if can_record_session then
+			local stat_name = "completed"
+			stat_name = stat_name .. "_" .. tostring(job_id)
+			stat_name = stat_name .. "_" .. tostring(Global.game_settings.difficulty)
+			stat_name = stat_name .. (Global.game_settings.one_down and "_od" or "")
+
+			if data.type == "victory" and managers.job:on_last_stage() and Global.statistics_manager.playing_from_start and not Global.blackmarket_manager.IngameAchievments[stat_name] then
+				Global.blackmarket_manager.IngameAchievments[stat_name] = os.time()
+			end
+		end
+	end)
 end
